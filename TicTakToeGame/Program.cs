b7ae11c7390d9e4cf29244330d899a8360f77db6
varun <varunlad5@gmail.com>
2021-09-28 11:50:44 +0530

    [varun] Welcome to TicTakToe Game

---
+++ TicTakToeGame/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicTakToeGame;
+
+namespace TicTakToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TikTakToeGame obj1 = new TikTakToeGame();
+            obj1.EmptyArray();
+        }
+    }
+}
---
+++ TicTakToeGame/TikTakToeGame.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTakToeGame
+{
+    public class TikTakToeGame
+    {
+        public void EmptyArray()
+        {
+            char[] board = new char[10];
+            for (int i = 1; i < board.Length+1; i++)
+            {
+                board[i] = ' ';
+            }           
+        }
+    }
+}
